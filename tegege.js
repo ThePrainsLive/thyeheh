2dd2dab368f68f9f14bf09da252086da25fce935
ThePrainsLive <112066074+ThePrainsLive@users.noreply.github.com>
2022-12-08 16:24:21 +0300

    tehgegehe.js
    gee

---
+++ tegege.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://biz.alibaba.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=alibaba.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+   var sattimer1 = setInterval(function () {
+     var divsToHide = document.getElementsByClassName("rax-view wapper"); //divsToHide is an array
+     for(var i = 0; i < divsToHide.length; i++){
+         divsToHide[i].style.visibility = "hidden"; // or
+         divsToHide[i].style.display = "none"; // depending on what you're doing
+     }
+     }, 2000);
+})();
