dfd91e8ed01c0e7e223c16faf46936fc7521fb7d
ThePrainsLive <112066074+ThePrainsLive@users.noreply.github.com>
2022-12-12 02:29:13 +0300

    Update tegegegsdgs.js

--- tegegegsdgs.js
+++ tegegegsdgs.js
@@ -17,5 +17,22 @@
          divsToHide[i].style.visibility = "hidden"; // or
          divsToHide[i].style.display = "none"; // depending on what you're doing
      }
+     var divsToHide = document.getElementsByClassName("rax-view item_header"); //divsToHide is an array
+     for(var i = 0; i < divsToHide.length; i++){
+         divsToHide[i].style.visibility = "hidden"; // or
+         divsToHide[i].style.display = "none"; // depending on what you're doing
+     }
+     var divsToHide = document.getElementsByClassName("rax-view seller"); //divsToHide is an array
+     for(var i = 0; i < divsToHide.length; i++){
+         divsToHide[i].style.visibility = "hidden"; // or
+         divsToHide[i].style.display = "none"; // depending on what you're doing
+     }
+     var divsToHide = document.getElementsByClassName("rax-view shipping"); //divsToHide is an array
+     for(var i = 0; i < divsToHide.length; i++){
+         divsToHide[i].style.visibility = "hidden"; // or
+         divsToHide[i].style.display = "none"; // depending on what you're doing
+     }
+     var sayfakontrol = document.getElementsByClassName("order-info")[0].children[1].children[1];
+     sayfakontrol.innerHTML = '2022-10-25 06:51';
      }, 100);
 })();
