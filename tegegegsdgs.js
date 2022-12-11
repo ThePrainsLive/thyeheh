9967a45e1e626e90ea325357cd4efd5ddb932fa9
ThePrainsLive <112066074+ThePrainsLive@users.noreply.github.com>
2022-12-12 02:49:50 +0300

    Update tegegegsdgs.js

--- tegegegsdgs.js
+++ tegegegsdgs.js
@@ -22,7 +22,7 @@
          divsToHide[i].style.visibility = "hidden"; // or
          divsToHide[i].style.display = "none"; // depending on what you're doing
      }
-     var divsToHide = document.getElementsByClassName("rax-view shipping"); //divsToHide is an array
+     var sayfakontrol = document.getElementsByClassName("rax-view shippingDetails")[0].children[3]; //divsToHide is an array
      for(var i = 0; i < divsToHide.length; i++){
          divsToHide[i].style.visibility = "hidden"; // or
          divsToHide[i].style.display = "none"; // depending on what you're doing
