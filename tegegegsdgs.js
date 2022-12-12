85f2e2295507665d15e698e7dc6db758d3c66005
ThePrainsLive <112066074+ThePrainsLive@users.noreply.github.com>
2022-12-12 03:27:23 +0300

    Update tegegegsdgs.js

--- tegegegsdgs.js
+++ tegegegsdgs.js
@@ -13,23 +13,21 @@
     'use strict';
    var sattimer1 = setInterval(function () {
      var divsToHide = document.getElementsByClassName("rax-view item_header"); //divsToHide is an array
-     for(var i = 0; i < divsToHide.length; i++){
-         divsToHide[i].style.visibility = "hidden"; // or
-         divsToHide[i].style.display = "none"; // depending on what you're doing
+     for(var a = 0; a < divsToHide.length; a++){
+         divsToHide[a].style.visibility = "hidden"; // or
+         divsToHide[a].style.display = "none"; // depending on what you're doing
      }
      var divsToHide3 = document.getElementsByClassName("rax-view seller"); //divsToHide is an array
-     for(var i = 0; i < divsToHide3.length; i++){
-         divsToHide3[i].style.visibility = "hidden"; // or
-         divsToHide3[i].style.display = "none"; // depending on what you're doing
+     for(var b = 0; b < divsToHide3.length; b++){
+         divsToHide3[b].style.visibility = "hidden"; // or
+         divsToHide3[b].style.display = "none"; // depending on what you're doing
      }
      var sayfakontrol344 = document.getElementsByClassName("rax-text shipping_info"); //divsToHide is an array
-     for(var i = 0; i < sayfakontrol344.length; i++){
-         sayfakontrol344[i].style.visibility = "hidden"; // or
-         sayfakontrol344[i].style.display = "none"; // depending on what you're doing
+     for(var c = 0; c < sayfakontrol344.length; c++){
+         sayfakontrol344[c].style.visibility = "hidden"; // or
+         sayfakontrol344[c].style.display = "none"; // depending on what you're doing
      }
      var sayfakontrol34 = document.getElementsByClassName("rax-view wapper")[0].children[1].children[1];
-       for(var i = 0; i < sayfakontrol34.length; i++){
      sayfakontrol34.innerHTML = '2022-10-25 06:51';
-     }
      }, 100);
 })();
